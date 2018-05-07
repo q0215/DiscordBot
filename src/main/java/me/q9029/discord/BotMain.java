15cf41137dd4862b8c5e4a5c7d89faec74c4b839
q9029 <key.ken0215@gmail.com>
2018-05-08 01:37:18 +0900

    develop

---
+++ src/main/java/me/q9029/discord/BotMain.java
@@ -0,0 +1,80 @@
+package me.q9029.discord;
+
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.ICategory;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.MissingPermissionsException;
+
+public class BotMain {
+
+	private static Logger logger = LoggerFactory.getLogger(BotMain.class);
+
+	public static void main(String[] args) {
+
+		int exitCode = 0;
+		long startMillis = System.nanoTime();
+		try {
+			logger.info("Start.");
+
+			// get token
+			ResourceBundle bundle = ResourceBundle.getBundle("discord");
+			String token = bundle.getString("bot.token");
+
+			IDiscordClient client = null;
+			try {
+				// create built client
+				client = ClientUtil.getBuiltClient(token);
+
+				// client login
+				client.login();
+
+				// wait for establishing connection
+				long readyTimeoutMillis = Long.parseLong(bundle.getString("establish.timeout")) * 1000000000;
+				long startReadyMillis = System.nanoTime();
+				while (!client.isReady()) {
+					if (System.nanoTime() - startReadyMillis >= readyTimeoutMillis) {
+						throw new RuntimeException("The waiting time for establishing a connection has been exceeded.");
+					}
+				}
+
+				// send message
+				Long channelId = 0L;
+				IChannel channel = client.getChannelByID(channelId);
+				try {
+					channel.sendMessage("Hello world.");
+
+				} catch (MissingPermissionsException e) {
+					ICategory category = channel.getCategory();
+					logger.warn("Missing the permission to send messages. Guild_Name:" + channel.getGuild().getName()
+							+ (category != null ? " Category_Name:" + category.getName() : "") + " Channel_Name:"
+							+ channel.getName());
+
+				} catch (Exception e) {
+					logger.error("Failed to send messages. Channel_ID:" + channelId);
+				}
+
+			} finally {
+				// client logout
+				if (client != null && client.isLoggedIn()) {
+					client.logout();
+				}
+			}
+
+		} catch (Exception e) {
+			exitCode = 1;
+			logger.error("An unexpected exception occurred.", e);
+
+		} finally {
+			long endMillis = System.nanoTime();
+			logger.info("End.");
+			logger.info(endMillis - startMillis + " ns elapsed.");
+		}
+
+		System.exit(exitCode);
+	}
+}
---
+++ src/main/java/me/q9029/discord/ClientUtil.java
@@ -0,0 +1,18 @@
+package me.q9029.discord;
+
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+
+public class ClientUtil {
+
+	/**
+	 * Get new built discord client.
+	 * 
+	 * @param token
+	 *            discord token
+	 * @return client
+	 */
+	public static IDiscordClient getBuiltClient(String token) {
+		return new ClientBuilder().withToken(token).build();
+	}
+}
